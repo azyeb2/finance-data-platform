ba2563af163265012be13ef4b7593f57a6e699ef
Beyzanur Demir <dbeyzanur2@gmail.com>
2026-07-28 16:20:11 +0300

    Add main entry point for ETL pipeline

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Finance Data Platform is ready!")
+from src.etl_service.pipeline import run_etl_pipeline
+
+
+def main():
+
+    print("Finance Data Platform başlıyor...")
+
+    run_etl_pipeline()
+
+    print("Finance Data Platform tamamlandı.")
+
+
+if __name__ == "__main__":
+    main()
